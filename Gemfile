938db5c09a605aaf214675ad142cb1d4034207bb
emi ota <ametio.145@gmail.com>
2021-06-22 07:16:54 +0000

    ログイン機能を追加

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 #ruby '2.6.5'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'bcrypt'
 
 gem 'rails', '~> 5.2.5'
 gem 'pg', '>= 0.18', '< 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :posts do
-      collection do
-        post :confirm
-      end
+    collection do
+      post :confirm
     end
   end
+end
