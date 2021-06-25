e4973c7b888935ba994d139a797e3b1cac768f89
emi ota <ametio.145@gmail.com>
2021-06-25 02:05:50 +0000

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :posts do
